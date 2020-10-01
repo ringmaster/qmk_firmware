36ec9b375356c29fd1a1556f6b6d973e4e2d3f27
Owen Winkler <owinkler@covermymeds.com>
2020-09-30 22:17:01 -0400

    One key morse on Uno + QMK

--- keyboards/uno/keymaps/ringmaster/keymap.c
+++ keyboards/uno/keymaps/ringmaster/keymap.c
@@ -6,24 +6,28 @@ enum uno_keycode
 };
 
 static uint16_t pressTimer = 0xFFFF;
-#define CUSTOM_LONGPRESS 150
-#define CUSTOM_LONGERPRESS 750
-#define CUSTOM_STRING "I can put a whole buncha text in here and type it all with a single keypress."
+#define DOT_DASH_PRESS 200
+#define ENTER_PRESS 750
+#define CHAR_WAIT 350
+#define SPACE_WAIT 1000
 #define RESET_LENGTH 3000
 const uint8_t PROGMEM RGBLED_RAINBOW_MOOD_INTERVALS[] = { 10, 25, 50 };
 
 char stringToSend[2] = "a";
 char maxLetter = 'z';
+uint16_t code = 0;
+char letters[32] = "                               ";
 
 uint8_t presetCounter = 0;
+bool waiting_space = false;
 
 #define COUNTER X_A
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-      [0] = LAYOUT(
-            UNO
-          )
+    [0] = LAYOUT(
+        UNO
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -31,58 +35,77 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		case UNO:
             if (record->event.pressed) {
                 pressTimer = timer_read();
+                waiting_space = false;
+                if (code == 0) {
+                    code = 1;
+                }
             } else {
                 uint16_t timeElapsed = timer_elapsed(pressTimer);
-                switch (presetCounter) {
-                    case 0:
-                        SEND_STRING(SS_LCMD("n"));
-                    break;
-                    case 1:
-                        SEND_STRING("Hello!");
-                    break;
-                    case 2:
-                        SEND_STRING(SS_TAP(X_ENTER)SS_TAP(X_ENTER)"I am uno!");
-                    break;
-                    case 3:
-                        SEND_STRING(SS_TAP(X_ENTER)SS_TAP(X_ENTER)"I can do all sorts of useless things!");
-                    break;
-                    case 4:
-                        SEND_STRING(SS_TAP(X_ENTER)SS_TAP(X_ENTER)"And I have a built-in RGB LED!"SS_TAP(X_ENTER)SS_TAP(X_ENTER)SS_TAP(X_ENTER));
-                        rgblight_sethsv_noeeprom(255, 255, 255);
-                        rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD);
-                    break;
-                    default:
-                        if (timeElapsed < CUSTOM_LONGPRESS) {
-                            // Normal press.  We're going to send the current letter and increment the counter.
-                            SEND_STRING(SS_TAP(X_BSPACE));
-                            send_string(stringToSend);
-                            stringToSend[0]++;
-                            if (stringToSend[0] > maxLetter) {
-                                stringToSend[0] = 'a';
-                            }
-                        } else if (timeElapsed < CUSTOM_LONGERPRESS) {
-                            // Long press, confirm the current letter, reset counter
-                            stringToSend[0] = 'a';
-                            send_string(stringToSend);
-                        } else if (timeElapsed < RESET_LENGTH) {
-                            // Longer press, display macro.
-                            SEND_STRING(CUSTOM_STRING);
-                        } else {
-                            reset_keyboard();
-                        }
-                        presetCounter--;
-                    break;
+
+                if (timeElapsed < DOT_DASH_PRESS) {
+                    code = (code << 1);
+                } else if (timeElapsed < ENTER_PRESS) {
+                    code = (code << 1) + 1;
+                } else {
+                    SEND_STRING(SS_TAP(X_ENTER));
+                    code = 64;
                 }
-                presetCounter++;
+                if (code >= 64) {
+                    code = 0;
+                    waiting_space = false;
+                }
+                else {
+                    waiting_space = true;
+                    pressTimer = timer_read();
+                }
             }
         break;
     }
     return false;
 }
 
-void keyboard_post_init_user(void) {
-    rgblight_enable_noeeprom();
-    rgblight_sethsv_noeeprom(0, 0, 0);
-    rgblight_mode(1);
-    //rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD);
+void matrix_init_user(void) {
+    letters[0b101] = 'a';
+    letters[0b11000] = 'b';
+    letters[0b11010] = 'c';
+    letters[0b1100] = 'd';
+    letters[0b10] = 'e';
+    letters[0b10010] = 'f';
+    letters[0b1110] = 'g';
+    letters[0b10000] = 'h';
+    letters[0b100] = 'i';
+    letters[0b10111] = 'j';
+    letters[0b1101] = 'k';
+    letters[0b10100] = 'l';
+    letters[0b111] = 'm';
+    letters[0b110] = 'n';
+    letters[0b1111] = 'o';
+    letters[0b10110] = 'p';
+    letters[0b11101] = 'q';
+    letters[0b1010] = 'r';
+    letters[0b1000] = 's';
+    letters[0b11] = 't';
+    letters[0b1001] = 'u';
+    letters[0b10001] = 'v';
+    letters[0b1011] = 'w';
+    letters[0b11001] = 'x';
+    letters[0b11011] = 'y';
+    letters[0b11100] = 'z';
 }
+
+void matrix_scan_user(void) {
+    uint16_t timeElapsed = timer_elapsed(pressTimer);
+
+    if (waiting_space) {
+        if (timeElapsed > SPACE_WAIT) {
+            waiting_space = false;
+            SEND_STRING(" ");
+        }
+        else if (timeElapsed > CHAR_WAIT && code != 0) {
+            send_char(letters[code]);
+            code = 0;
+        }
+    }
+
+}
+
